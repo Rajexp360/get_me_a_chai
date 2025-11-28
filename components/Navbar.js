eb57a84d92a85079906255f20795b2d6dfeadf29
rajexp360 <rajex369@gmail.com>
2025-11-28 16:05:52 +0545

    add dashboard navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,29 +23,30 @@ const Navbar = () => {
           <div className='flex items-center'>
             <div className="relative mx-2">
               <button onClick={() => setshowdropdown(!showdropdown)} id="dropdownDelayButton" data-dropdown-toggle="dropdownDelay" data-dropdown-delay="500" data-dropdown-trigger="hover" className="inline-flex items-center bg-linear-to-r cursor-pointer from-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-6 rounded-xl shadow-lg transform transition duration-500 hover:scale-105 focus:outline-none" type="button">
-               {session.user.name}
-                <svg className="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/></svg>
+                {session.user.name}
+                <svg className="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" /></svg>
               </button>
-              
-                <div className={`absolute z-10 mt-2 left-1.5 bg-gray-800 rounded-lg shadow-lg w-44 origin-top-right transition-all duration-300 ease-out ${showdropdown ? 'opacity-100 scale-100 visible' : 'opacity-0 scale-95 invisible'}`}>
-                  <ul className="py-2 text-sm text-gray-700">
-                    <li>
-                      <a href="#" className="block ml-2 py-2 text-white transition-transform duration-400 hover:scale-105">Contact</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block ml-2 py-2 text-white transition-transform duration-400 hover:scale-105">Settings</a>
-                    </li>
-                   <li>
-                      <a href="#" className="block ml-2 py-2 text-white transition-transform duration-400 hover:scale-105">Profile</a>
-                    </li>
-                  </ul>
-                </div>
-              
+
+              <div className={`absolute z-10 mt-2 left-1.5 bg-gray-800 rounded-lg shadow-lg w-44 origin-top-right transition-all duration-300 ease-out ${showdropdown ? 'opacity-100 scale-100 visible' : 'opacity-0 scale-95 invisible'}`}>
+                <ul className="py-2 text-sm text-gray-700">
+                  <li>
+                    <Link href="#" className="block ml-4 py-2 text-white transition-transform duration-400 hover:scale-105">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link href="#" className="block ml-4 py-2 text-white transition-transform duration-400 hover:scale-105">Settings</Link>
+                  </li>
+                  <li>
+                    <Link href="#" className="block ml-4 py-2 text-white transition-transform duration-400 hover:scale-105">Earnings</Link>
+                  </li>
+                  <li>
+                    <Link onClick={() => signOut()} href="#" className="block ml-4 py-2 text-white transition-transform duration-400 hover:scale-105">Sign out</Link>
+                  </li>
+                </ul>
+              </div>
+
             </div>
-            
-            <Link href="/profile" className="inline-block bg-linear-to-r cursor-pointer from-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-6 rounded-xl shadow-lg transform transition duration-500 hover:scale-105 ">
-              Profile
-            </Link>
+
+         
             <button onClick={() => signOut()} className="bg-linear-to-r cursor-pointer from-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-6 rounded-xl shadow-lg transform transition duration-500 hover:scale-105 m-2">Sign out</button>
           </div>
         ) : (
