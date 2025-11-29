e828185db64b5ddc47514ab6548f9c4960239f5b
rajexp360 <rajex369@gmail.com>
2025-11-29 14:03:56 +0545

    feat: implement dropdown close on outside click

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Link from 'next/link'
 import { useSession, signIn, signOut } from "next-auth/react"
@@ -8,6 +8,23 @@ import { useSession, signIn, signOut } from "next-auth/react"
 const Navbar = () => {
   const [showdropdown, setshowdropdown] = useState(false)
   const { data: session } = useSession()
+  const dropdownRef = useRef(null)
+  useEffect(() => {
+    function handleClickOutside(event){
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setshowdropdown(false);
+      }
+    }
+     document.addEventListener("mousedown", handleClickOutside);
+
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [dropdownRef])
+  
+
+
 
   return (
     <nav className='bg-gray-900 text-white flex justify-between items-center px-4 h-16'>
@@ -21,7 +38,7 @@ const Navbar = () => {
       <div>
         {session ? (
           <div className='flex items-center'>
-            <div className="relative mx-2">
+            <div ref={dropdownRef} className="relative mx-2">
               <button onClick={() => setshowdropdown(!showdropdown)} id="dropdownDelayButton" data-dropdown-toggle="dropdownDelay" data-dropdown-delay="500" data-dropdown-trigger="hover" className="inline-flex items-center bg-linear-to-r cursor-pointer from-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-6 rounded-xl shadow-lg transform transition duration-500 hover:scale-105 focus:outline-none" type="button">
                 {session.user.name}
                 <svg className="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" /></svg>
@@ -46,7 +63,7 @@ const Navbar = () => {
 
             </div>
 
-         
+
             <button onClick={() => signOut()} className="bg-linear-to-r cursor-pointer from-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-6 rounded-xl shadow-lg transform transition duration-500 hover:scale-105 m-2">Sign out</button>
           </div>
         ) : (
